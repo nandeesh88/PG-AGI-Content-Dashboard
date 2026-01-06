08683ad77ee97f7c3fb1fe9a1f2d5aa98d5b7aac
nandeesh88 <nandugowda888@gmail.com>
2026-01-06 18:15:57 +0530

    Initial commit - PG-AGI Content Dashboard

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos', 'image.tmdb.org', 'pbs.twimg.com'],
   },
-  experimental: {
-    serverActions: true,
-  },
 }
 
 module.exports = nextConfig
